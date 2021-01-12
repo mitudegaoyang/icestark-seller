4383842c73c493d3a576c87d1399e0aa87bf1335
迷途的羔羊 <13683265113@163.com>
2021-01-12 17:41:54 +0800

    feat: 微应用间跳转

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,67 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import IceContainer from '@icedesign/container';
 import { Button } from '@alifd/next';
-import { Link, useHistory } from 'ice';
+import { Link } from 'ice';
+import { appHistory } from '@ice/stark-app';
+import { AppLink } from '../../utils';
 import PageTitle from '@/components/PageTitle';
 
 import styles from './index.module.scss';
 
-const mockData = () => {
-  return {
-    status: 'SUCCESS',
-    message: '请求成功',
-    data: Array.from({ length: 10 }).map((item, index) => {
-      return {
-        id: `00000${index}`,
-        name: '聘用合同',
-        ourCompany: 'xxx商铺',
-        amount: '999,999',
-        currency: 'CNY',
-        state: '签约中',
-      };
-    }),
-  };
-};
-
 export default function List() {
-  const [isLoading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
-  const [current, setCurrent] = useState(1);
-
-  async function handlePagination(currentPage) {
-    await setCurrent(currentPage);
-  }
-
-  useEffect(() => {
-    async function fetchData() {
-      await setLoading(true);
-      const { data: resData } = await mockApi();
-      await setData(Array.isArray(resData) ? resData : []);
-      await setLoading(false);
-    }
-    fetchData();
-  }, [current]);
-
-  function mockApi() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve(mockData());
-      }, 600);
-    });
-  }
 
   function handleHistoryPush() {
-    let history = useHistory();
-    console.log(history)
-    // history.push("/message");
+    appHistory.push('/angular');
   };
 
   return (
     <IceContainer>
       <PageTitle title="商家首页" />
-      <Link className={styles.link} to="/list">子应用内跳转</Link>
+      <Link className={styles.link} to="/list">Link子应用内跳转</Link>
       <br />
-      <Button onClick={handleHistoryPush}>子应用间跳转 </Button>
+      <br />
+      <AppLink className={styles.link} to="/waiter">AppLink子应用间跳转</AppLink>
+      <br />
+      <br />
+      <Button type="primary" onClick={handleHistoryPush}>appHistory子应用间跳转 </Button>
     </IceContainer>
   );
 }
